50d3d5792e29bdc679fba1ba133cc5328b4a56f5
bdyuvu <1689981879@qq.com>
2022-06-21 03:30:13 +0800

    js

---
+++ JavaScript/js.js
@@ -0,0 +1,68 @@
+// // 输入框
+// prompt('qkuuslkdjfk')
+//     // 警示框
+// alert('sodiowe')
+//     // 控制台输出
+// console.log('随便')
+
+// // 声明变量
+// var age;
+// // 赋值
+// age = 1996;
+// // 打印
+// console.log(age);
+
+// 变量的初始化
+// var myname = 'yimk';
+// console.log(myname);
+
+// 小示例
+// var myname = prompt('请输入你的名字');
+// alert(myname);
+
+// 更新变量
+// var myname = 'yimk';
+// // console.log(myname);
+// // 声明多个变量
+// var myname = 'yi',
+//     age = 19,
+//     gs = lk;
+// console.log(myname);
+
+// var age = '14';
+// console.log(age);
+
+// prompt('wode')
+
+// 临时变量， temp， 交换数值
+// var temp;
+// var apple1 = 'q';
+// var apple2 = 'h';
+// temp = apple1;
+// apple1 = apple2;
+// apple2 = temp;
+// console.log(apple1);
+// console.log(apple2);
+
+// console.log(-Number.MAX_VALUE * 2);
+// console.log(Number.MIN_VALUE);
+
+// console.log(isNaN('kjs' / 19));
+
+// alert('酷热，送来肯教 \b owie。\n 的：“收破\'ui\'烂了”。\t slkdfjweo. 。。、、\\')
+
+
+// var wodi = 'dfssdfwef'
+// var wodi = 163546
+// console.log('wo' + wodi + 'jkk');
+
+// var age = prompt('年龄');
+// alert('您今年' + age + '岁')
+// console.log(typeof age);
+
+// var diyi = prompt();
+// var dier = prompt();
+// var result = Number(diyi) + Number(dier)
+// alert('jpgo:' + result);
+
+// console.log(135 % 9);
