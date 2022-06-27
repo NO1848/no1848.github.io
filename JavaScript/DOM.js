c357598ba03bb8da5a9f8f002bc745bef388212f
bdyuvu <1689981879@qq.com>
2022-06-27 10:52:33 +0800

    DOM

--- JavaScript/DOM.js
+++ JavaScript/DOM.js
@@ -1,1 +1,13 @@
 // DOM
+
+// var lis = document.getElementsByTagName('li');
+// // console.log(lis[1]);
+// console.dir(lis);
+
+// 测试下电脑性能
+// for (var i = 1; i > 0; i++) {
+//     console.log(i);
+// }
+
+// var idd=document.querySelector(#id);
+// var classs=document.querySelectorAll(.li);
